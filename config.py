4e5ebd5b4e148155493fbe1035b21560a8a15542
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2023-10-31 21:43:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001774060800"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hey, {mention}! 😃\n\nThis is Royal The Permanent File Store Bot. 🤖\n\nI am Working For <a href='t.me/wombackup'>This Channel</a></b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hey, {mention} How Are You? \n\Im Royal The Permanent Any File Store Bot!</b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "1782834874").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hey, {mention}\n\nYou need to join in my Channel to use me! Kindly Please join Channel From Below Link ✅</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Sorry, {mention} 😶\n\n<i>Due To Overload Only Channel Subscribers Can Use Me Kindly Join My Channel To Access All Movies/Series File ✅</i></b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>{filename}\n\n         ❤ We Love You ❤\n🔥 ➹ Join Now [ <a href='t.me/wombackup'>WOM-BACKUP</a> ] ➷ 🔥</b>")
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", False) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "<b>❌ I am Work's Only For <a href='https://t.me/wombackup'>This Channel!</a>\n\nFor More Query Contact My <a href='t.me/royaldwip'>Devoloper!</a> ✅</b>"
+USER_REPLY_TEXT = "<b>Dear, {mention}\n\n<i>According To My Database You Don't Have Any Specific Rights To Use Me!<i></b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
